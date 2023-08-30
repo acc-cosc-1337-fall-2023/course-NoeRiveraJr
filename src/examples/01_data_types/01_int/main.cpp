d44f660cd124b0c8e18be631045bca4b1bde738b
Noe Rivera Jr <noe.rivera.jr@outlook.com>
2023-08-30 00:49:07 +0000

    Intro to dev process, int, unit tests

--- src/examples/01_data_types/01_int/main.cpp
+++ src/examples/01_data_types/01_int/main.cpp
@@ -1,5 +1,20 @@
+#include<iostream>
+#include "int.h" //I have access to the echo_variable function in my library
+
+using std::cout;
+
 int main()
 {
+    int num =5;
+
+    int result = echo_variable(num);
+
+    cout<<"Result: "<<result<<"\n";
+    cout<<"Num: "<<num<<"\n";
+
+    num = 100;
+
+    cout<<"Num: "<<num<<"\n";
 
     return 0;
 }
