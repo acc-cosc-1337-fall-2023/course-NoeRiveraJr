d5e05835fcd1394e14a8d4b922d224e0d09d4032
Noe Rivera Jr <noe.rivera.jr@outlook.com>
2023-09-15 00:37:16 +0000

    intro to while loop

--- src/examples/04_repetition/01_while/main.cpp
+++ src/examples/04_repetition/01_while/main.cpp
@@ -1,7 +1,16 @@
 #include "while.h"
 
+using std::string;
+using std::cout;
+
 int main() 
 {
+	string str = "Hello";
 
+	display_string(str);
+
+	auto num = 5;
+	auto result = sum_of_squares(num);
+	cout<<"Result is: "<<result<<"\n";	
 	return 0;
 }
