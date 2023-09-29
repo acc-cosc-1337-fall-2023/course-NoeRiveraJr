a0f9643114d1b83566a7b6b67203d9c861d8ee23
Noe Rivera Jr <noe.rivera.jr@outlook.com>
2023-09-29 00:26:54 +0000

    intro to vectors

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -1,4 +1,21 @@
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::string;
+
 int main()
 {
+    string lang = "C++";
+    
+    cout<<lang<<"\n";
+    cout<<"Address of lang: "<<&lang<<"\n";
+
+
+    cout<<lang[0]<<"\n";
+    
+
+
+
     return 0;
 }
--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -1,4 +1,39 @@
+#include<vector>
+#include<iostream>
+
+using std::cout;
+using std::vector;
+
 int main()
 {
+    vector<int> nums{7,4,20};
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+    cout<<"Size: "<<nums.size()<<"\n";
+    
+    for (auto i = 0; i < nums.size(); i++)
+    {
+        cout<<nums[i]<<"\n";
+    }
+
+    nums.push_back(3);
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+    cout<<"Size: "<<nums.size()<<"\n";
+
+    for(auto num: nums)
+    {
+        cout<<num<<"\n";
+    }
+
+    nums.push_back(25);
+    nums.push_back(20);
+
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+    cout<<"Size: "<<nums.size()<<"\n";
+    
+    nums.push_back(20);
+
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+    cout<<"Size: "<<nums.size()<<"\n";
+
     return 0;
 }
