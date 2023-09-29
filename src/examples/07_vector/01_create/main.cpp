844c4fb6669b21f5355204145108b2af8e21e40d
Noe Rivera Jr <noe.rivera.jr@outlook.com>
2023-09-29 00:32:56 +0000

    double vector creation

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -7,33 +7,27 @@ using std::vector;
 int main()
 {
     vector<int> nums{7,4,20};
-    cout<<"Capacity: "<<nums.capacity()<<"\n";
-    cout<<"Size: "<<nums.size()<<"\n";
-    
-    for (auto i = 0; i < nums.size(); i++)
+    vector<int> nums1;
+    vector<int> nums2(5,10);
+
+    for(auto num2: nums2)
     {
-        cout<<nums[i]<<"\n";
+        cout<<num2<<"\n";
     }
 
-    nums.push_back(3);
-    cout<<"Capacity: "<<nums.capacity()<<"\n";
-    cout<<"Size: "<<nums.size()<<"\n";
+    vector<int> nums3 = nums;
 
-    for(auto num: nums)
+    for(auto num3: nums3)
     {
-        cout<<num<<"\n";
+        cout<<num3<<"\n";
     }
 
-    nums.push_back(25);
-    nums.push_back(20);
-
-    cout<<"Capacity: "<<nums.capacity()<<"\n";
-    cout<<"Size: "<<nums.size()<<"\n";
-    
-    nums.push_back(20);
-
-    cout<<"Capacity: "<<nums.capacity()<<"\n";
-    cout<<"Size: "<<nums.size()<<"\n";
+    vector<double> doubles {5.5,3.9,8.99};
+    cout<<"\n";
+    for(auto dub: doubles)
+    {
+        cout<<dub<<"\n";
+    }
 
     return 0;
 }
