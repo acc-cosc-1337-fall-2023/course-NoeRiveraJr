7b5d4ad6ea1654128b2192a7bbee80934efa6418
Noe Rivera Jr <noe.rivera.jr@outlook.com>
2023-10-04 00:57:13 +0000

    Intro to classes

--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -1,1 +1,16 @@
 //bank_account.h
+#include<iostream>
+
+class BankAccount
+{
+    public:
+        //constructor
+        BankAccount(int b) : balance(b){/*empty class function*/} //constructor
+        int get_balance(){return balance;}
+    private:
+        int balance{0};
+
+};
+
+
+void say_hello();
